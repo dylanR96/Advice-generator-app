81c727eb6dcf72e292f3f0a587ccbe51277385a4
dylanR96 <dylan.ravenson@gmail.com>
2024-02-11 14:06:47 +0100

    Added error handling for api and finishing touches

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// https://api.adviceslip.com/advice
 const url = "https://api.adviceslip.com/advice";
 const adviceNumContainer = document.getElementById("advice-number");
 const adviceContainer = document.getElementById("advice");
 const diceButton = document.getElementById("dice-button");
 
 async function myApi() {
-  const response = await fetch(url);
-  const {slip} = await response.json();
-  const adviceId = slip.id;
-  const myAdvice = slip.advice;
-  adviceNumContainer.innerHTML = `ADVICE #${adviceId}`;
-  adviceContainer.innerHTML = `"${myAdvice}"`;
+  try {
+    const response = await fetch(url);
+    const { slip } = await response.json();
+    const adviceId = slip.id;
+    const myAdvice = slip.advice;
+    adviceNumContainer.textContent = `ADVICE #${adviceId}`;
+    adviceContainer.textContent = `"${myAdvice}"`;
+  } catch (error) {
+    console.error("Error: " + error);
+    adviceContainer.textContent = "Woops! An error has occured. Please try again later!";
+  }
+
 }
 
 myApi();
 
 diceButton.addEventListener("click", myApi);
-
-// {
-//   "slip": {
-//       "id": 198,
-//       "advice": "Sing in the shower."
-//   }
-// }
